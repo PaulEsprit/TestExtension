b4372aa1a04091a69d29dbc2b621853fa0b46637
Yuriy D <ydovhopol@solvexus.com>
2024-12-07 17:40:17 +0200

    no additional tab

--- mic-and-tab/background.js
+++ mic-and-tab/background.js
@@ -5,25 +5,27 @@ chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
                 resolve(streamId);
             });
         });
-        console.error('BACKGROUND StreamId', streamId);
-        chrome.tabs.create({ url: 'https://deepgram.com' }, async (newTab) => {
-          // await chrome.scripting.executeScript({
-          //   target: { tabId: newTab.id },
-          //   files: ["content-script.js"]
-          // });
+        await new Promise(resolve => setTimeout(resolve, 3000));
+        await chrome.tabs.sendMessage(message.tabId, { message: 'start', streamId });
+        // console.error('BACKGROUND StreamId', streamId);
+        // chrome.tabs.create({ url: 'https://deepgram.com' }, async (newTab) => {
+        //   // await chrome.scripting.executeScript({
+        //   //   target: { tabId: newTab.id },
+        //   //   files: ["content-script.js"]
+        //   // });
 
-          async function sendMessageAfterTimeout() {
-            try {
-              await new Promise(resolve => setTimeout(resolve, 5000));
-              await chrome.tabs.sendMessage(newTab.id, { message: 'start', streamId });
-              console.error('Message sent to content script in new tab.');
-            } catch (error) {
-              console.error('Error sending message to content script:', error);
-            }
-          }
+        //   async function sendMessageAfterTimeout() {
+        //     try {
+        //       await new Promise(resolve => setTimeout(resolve, 5000));
+        //       await chrome.tabs.sendMessage(newTab.id, { message: 'start', streamId });
+        //       console.error('Message sent to content script in new tab.');
+        //     } catch (error) {
+        //       console.error('Error sending message to content script:', error);
+        //     }
+        //   }
         
-          await sendMessageAfterTimeout();
-        });
+        //   await sendMessageAfterTimeout();
+        // });
     }
     else if (message.message === 'createRecord') {
       await createRecord(message.payload);

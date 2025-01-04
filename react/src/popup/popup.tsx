8415cfeda12ce0f639c889925ac5057237e74e11
Yuriy D <ydovhopol@solvexus.com>
2025-01-04 20:59:46 +0200

    structure

--- react/src/popup/popup.tsx
+++ react/src/popup/popup.tsx
@@ -1,3 +1,28 @@
-import React from 'react'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './popup.css'
 
-const test = <p>Hello World!</p>
+// Create a React element
+const popup = 
+<div>
+    <div id="header">
+      <button id="options">Options</button>
+      <p id="recording">Recording...</p>
+    </div>
+    <div id="controls">
+      <button id="start">Start</button>
+      <button id="stop">Stop</button>
+      <button id="clear">Clear</button>
+    </div>
+    <p id="transcript"></p>
+</div>;
+
+// Create a new div element and append it to the body
+const doc = document.createElement('div');
+document.body.appendChild(doc);
+
+// Create a root for React using the div element
+const root = ReactDOM.createRoot(doc);
+
+// Render the React element inside the root
+root.render(popup);
--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,11 +1,18 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { type } = require('os');
 
 module.exports = {
     mode: 'development',
+    devtool: 'cheap-module-source-map',
     entry: {
-        popup: path.resolve('src/popup/popup.tsx')
+        popup: path.resolve('src/popup/popup.tsx'),
+        options: path.resolve('src/options/options.tsx'),
+        offscreen: path.resolve('src/offscreen/offscreen.tsx'),
+        welcome: path.resolve('src/welcome/welcome.tsx'),
+        background: path.resolve('src/background/background.ts'),
+        contentScript: path.resolve('src/contentScript/contentScript.ts')
     },
     module: {
         rules: [
@@ -14,22 +21,26 @@ module.exports = {
                 use: 'ts-loader',
                 exclude: /node_modules/,
             },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                type: 'assets/resource',
+                test: /\.(png|svg|jpg|jpeg|gif)$/,
+            }
         ],
     },
     plugins : [
         new CopyPlugin({
             patterns: [
                 { 
-                    from: path.resolve('src/manifest.json'),
+                    from: path.resolve('src/static'),
                     to: path.resolve('dist') 
                 },
             ],
         }),
-        new HtmlWebpackPlugin({
-            title: 'Meet-Track-AI',
-            filename: 'popup.html',
-            chunks: ['popup']
-        })
+        ...getHtmlPlugins(['popup', 'options', 'offscreen', 'welcome'])
     ],
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
@@ -37,5 +48,20 @@ module.exports = {
     output: {
         filename: '[name].js',
         path: path.resolve('dist'),
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all'
+        }
     }
+}
+
+function getHtmlPlugins(chnks) {
+    return chnks.map(chunk => {
+        return new HtmlWebpackPlugin({
+            title: 'Meet-Track-AI',
+            filename: `${chunk}.html`,
+            chunks: [chunk]
+        })
+    })
 }
